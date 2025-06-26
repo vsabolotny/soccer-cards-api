a8f5b054297d689324d18ada184ff229f057ea2c
Vladislav Sabolotny <vladislav.sabolotny@gmail.com>
2025-06-26 23:17:40 +0200

    removed from config to .env

--- config.py
+++ config.py
@@ -1,22 +1,35 @@
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
 load_dotenv()
 
+# Define the base directory of the project
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+print(f"basedir: {basedir}")
+
+# Ensure the database directory exists
+db_dir = os.path.join(basedir)
+if not os.path.exists(db_dir):
+    os.makedirs(db_dir)
+
+# Define the default database path
 db_path = os.path.join(basedir, 'soccer_cards_app.db')
 
+print(f"Database path: {db_path}")
+
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'you_should_change_this')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', f'sqlite:///{db_path}')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'ba5d152a234ae39176e5d1ed04e3396ab0188e4264c08d2db36ddb10449075ef')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
     UPLOAD_FOLDER = os.path.join(basedir, 'app', 'static', 'uploads') 
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv('TEST_JWT_SECRET_KEY', 'test_jwt_secret_key')
+    JWT_SECRET_KEY = os.getenv('TEST_JWT_SECRET_KEY')
     UPLOAD_FOLDER = os.path.join(basedir, 'app', 'static', 'uploads_test')
     WTF_CSRF_ENABLED = False
